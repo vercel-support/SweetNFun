ed5229e19a915aa02f5e39591ebc36beb4aba071
413XPK <alexparktoronto@gmail.com>
2022-10-21 17:27:12 -0400

    styling and form-submit

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ $(".carousel").owlCarousel({
     margin: 20,
     loop: true,
     autoplay: true,
+    
     // autoplayTimeout: 2000,
     autoplayHoverPause: true,
+    
     responsive: {
         0: { items: 1, nav: false },
-        1600: { items: 2, nav: false },
-        2000: { items: 3, nav: false },
-        2000: { items: 4, nav: false },
-        2000: { items: 5, nav: false },
+        1600: { items: 1, nav: false },
     },
   });
+
+  
 
   
 
