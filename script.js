a848f70e04498057c09f2b17ede4bdd0a9f6ba96
413XPK <alexparktoronto@gmail.com>
2022-11-08 17:23:15 -0500

    nav bar search start

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+//search bar anim
+
+const searchIcon = document.querySelector('.searchimged');
+const searchPart = document.querySelector('.search-part');
+const searchInput = document.querySelector('.search-input');
+// const closeSearch = document.querySelector
+
+// const onClickOutside = (e) => {
+//   if (!e.target.className.includes('change')) {
+//     show = false;
+//   }
+// };
+
+
+  searchIcon.addEventListener('click', function handleClick(event){
+    searchIcon.parentElement.classList.add('change');
+    searchPart.classList.add('change');
+  
+    setTimeout(() => {
+      searchInput.focus();
+    }, 0)
+
+  })
+
+
+  // if (searchPart.contains.classList('change')){
+  //   searchIcon.addEventListener('click', ()=>{
+  //     searchPart.classList.remove('change');
+  //     searchIcon.parentElement.classList.remove('change');
+  //   })
+  // }
+
+
+window.removeEventListener('click', handleClick)
+
+
+
 //hero carousel start
 $(".carousel").owlCarousel({
     margin: 20,
@@ -13,13 +50,10 @@ $(".carousel").owlCarousel({
     },
   });
 
-  
+//hero carousel end//
 
-  
 
-  //hero carousel end
-
-  //popular products slide start
+//popular products slide start
 
 const gap = 16;
 
@@ -50,4 +84,5 @@ prev.addEventListener('click', (e) => {
 let width = carousel.offsetWidth;
 window.addEventListener('resize', (e) => (width = carousel.offsetWidth));
 
-//hero carousel end
+//hero carousel end //
+
